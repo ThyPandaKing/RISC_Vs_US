142151097ca258a77253ab3b464c891c21897bfe
pandaking <aditya9660sharma@gmail.com>
2022-09-09 21:10:13 +0530

    saving in file implemented and integrating backend and frontend finished

--- Tiny_OS/main.js
+++ Tiny_OS/main.js
@@ -1,23 +1,33 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow, ipcMain} = require ('electron');
+const path = require ('path');
+var fs = require ('fs');
 
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  const mainWindow = new BrowserWindow ({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      contextIsolation: true,
+      nodeIntegration: true,
+      preload: path.resolve ('./preload.js'),
+    },
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
+  mainWindow.loadFile ('index.html');
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
+
+ipcMain.on ('saveCode', (event, myCode) => {
+  console.log (myCode);
+  fs.writeFile ('../User/userCode.txt', myCode, function (err) {
+    if (err) throw err;
+    console.log ('Saved!');
+  });
+});
 
 // function hi(){
 //   console.log("hey");
@@ -26,25 +36,24 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+app.whenReady ().then (() => {
+  createWindow ();
 
-  app.on('activate', function () {
+  app.on ('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows ().length === 0) createWindow ();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on ('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit ();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
 
 // export {hi};
--- Tiny_OS/preload.js
+++ Tiny_OS/preload.js
@@ -1,12 +1,28 @@
+const {ipcRenderer} = require ('electron');
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+window.addEventListener ('DOMContentLoaded', () => {
+  let saveButton = document.getElementById ('saveButton');
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+  saveButton.addEventListener ('click', () => {
+    let code = document.getElementById ('hiddenCodeWriter');
+    const defaultCode = `int main(){
+  /*Write your code here*/
+  return -1;
+}`;
+
+    console.log (code.value);
+    const myCode = code.value == '' ? defaultCode : code.value;
+
+    ipcRenderer.send ('saveCode', myCode);
+  });
+
+  // const replaceText = (selector, text) => {
+  //   const element = document.getElementById(selector)
+  //   if (element) element.innerText = text
+  // }
+
+  // for (const type of ['chrome', 'node', 'electron']) {
+  //   replaceText(`${type}-version`, process.versions[type])
+  // }
+});
