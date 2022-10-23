e1a9e50887bcc16a5be4294d0b764d3bc8f3a6f2
BobbaRuthvik <73455820+BobbaRuthvik@users.noreply.github.com>
2022-10-23 18:32:28 +0530

    vm conversion 4

--- Compiler/tac-vm.cpp
+++ Compiler/tac-vm.cpp
@@ -148,11 +148,7 @@ void conversion(){
                 vm.push_back("pop " + type_a.second + " " + to_string(type_a.first.first) + " " + type_a.first.second);
             }
             else if(tac[i].size() == 7){
-                vm.push_back("push constant 1 INT");
                 pair<pair<int, string>, string> type_a = get_type(tac[i][1], "null");
-                vm.push_back("push " + type_a.second + " " + to_string(type_a.first.first) + " " + type_a.first.second);
-                vm.push_back("Eq");
-                vm.push_back("if-goto " + tac[i][3]);
                 vm.push_back("push constant 0 INT");
                 vm.push_back("push " + type_a.second + " " + to_string(type_a.first.first) + " " + type_a.first.second);
                 vm.push_back("Eq");
