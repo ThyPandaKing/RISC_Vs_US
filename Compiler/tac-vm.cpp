35350bc4dac3796bdd6bc4b1ecd7141dbb21b3cc
Kishor783 <gddmkishor@gmail.com>
2022-11-13 16:00:11 +0530

    char constants issue resolved

--- Compiler/tac-vm.cpp
+++ Compiler/tac-vm.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 vector<vector<string>> tac;
 vector<string> vm;
-map<string, pair<float, string>> constant;
-map<string, pair<int, string>> local;
+map<string, pair<string, string>> constant;
+map<string, pair<string, string>> local;
 int local_idx = 0;
-map<string, pair<int, string>> argument;
+map<string, pair<string, string>> argument;
 int arg_idx = 0;
-map<string, pair<int, string>> temp;
+map<string, pair<string, string>> temp;
 int temp_idx = 0;
 map<string, string> op_map;
 map<string, int> strings;
@@ -31,11 +31,11 @@ void initialize(){
 
 vector<string> tokenize(string in){
     vector<string> res;
-    string temp = "";
+    string temp_t = "";
     for(int i=0; i<in.size(); i++){
         if(in[i] == ' '){
-            res.push_back(temp);
-            temp = "";
+            res.push_back(temp_t);
+            temp_t = "";
             while(i<in.size() && in[i] == ' '){
                 i++;
             }
@@ -53,15 +53,15 @@ vector<string> tokenize(string in){
                     l++;
                 }
 
-                temp = str.substr(0,str.length()-l+1);
+                temp_t = str.substr(0,str.length()-l+1);
             }
             else{
-                temp += in[i];
+                temp_t += in[i];
             }
         }
     }
-    if(temp.size())
-        res.push_back(temp);
+    if(temp_t.size())
+        res.push_back(temp_t);
     return res;
 }
 
@@ -88,20 +88,21 @@ bool isOperator(string op){
     return false;
 }
 
-pair<pair<int, string>, string> get_type(string var, string type){
-    pair<pair<int, string>, string> temp_var;
+pair<pair<string, string>, string> get_type(string var, string type){
+    pair<pair<string, string>, string> temp_var;
     if(var[0] == '@'){
         if(temp.find(var) == temp.end()){
-            temp[var].first = temp_idx++;
+            temp[var].first = to_string(temp_idx);
             temp[var].second = type;
+            temp_idx++;
         }
         temp_var.first.first = temp[var].first;
         temp_var.first.second = temp[var].second;
         temp_var.second = "temp";
     }
-    else if(isNumber(var)){
+    else if(isNumber(var) || var[0] == '\''){
         if(constant.find(var) == constant.end()){
-            constant[var].first = stof(var);
+            constant[var].first = var;
             constant[var].second = type;
             // check for the constant type
         }
@@ -117,8 +118,9 @@ pair<pair<int, string>, string> get_type(string var, string type){
         }
         else{
             if(local.find(var) == local.end()){
-                local[var].first = local_idx++;
+                local[var].first = to_string(local_idx);
                 local[var].second = type;
+                local_idx++;
             }
             temp_var.first.first = local[var].first;
             temp_var.first.second = local[var].second;
@@ -154,14 +156,14 @@ void conversion(){
         }
         if(tac[i].size() > 1){
             if(tac[i].size() == 6 and tac[i][1] == "=" and isOperator(tac[i][3])){
-                pair<pair<int, string>, string> type_a = get_type(tac[i][0], tac[i][5]);
-                pair<pair<int, string>, string> type_b = get_type(tac[i][2], tac[i][5]);
-                pair<pair<int, string>, string> type_c = get_type(tac[i][4], tac[i][5]);
-                vm.push_back("push " + type_b.second + " " + to_string(type_b.first.first) + " " + type_b.first.second);
-                vm.push_back("push " + type_c.second + " " + to_string(type_c.first.first) + " " + type_c.first.second);
+                pair<pair<string, string>, string> type_a = get_type(tac[i][0], tac[i][5]);
+                pair<pair<string, string>, string> type_b = get_type(tac[i][2], tac[i][5]);
+                pair<pair<string, string>, string> type_c = get_type(tac[i][4], tac[i][5]);
+                vm.push_back("push " + type_b.second + " " + type_b.first.first + " " + type_b.first.second);
+                vm.push_back("push " + type_c.second + " " + type_c.first.first + " " + type_c.first.second);
                 vm.push_back(op_map[tac[i][3]] + " " + tac[i][5]);
                 if(op_map[tac[i][3]] != "eq")
-                    vm.push_back("pop " + type_a.second + " " + to_string(type_a.first.first) + " " + type_a.first.second);
+                    vm.push_back("pop " + type_a.second + " " + type_a.first.first + " " + type_a.first.second);
             }
             else if(tac[i].size() == 2){
                 if(tac[i][0] == "GOTO"){
@@ -178,8 +180,8 @@ void conversion(){
             }
             else if(tac[i].size() == 3){
                 if(tac[i][0] == "return"){
-                    pair<pair<int, string>, string> type_a = get_type(tac[i][1], tac[i][2]);
-                    vm.push_back("push " + type_a.second + " " + to_string(type_a.first.first) + " " + type_a.first.second);
+                    pair<pair<string, string>, string> type_a = get_type(tac[i][1], tac[i][2]);
+                    vm.push_back("push " + type_a.second + " " + type_a.first.first + " " + type_a.first.second);
                     vm.push_back("pop argument 0 " + type_a.first.second);
                     vm.push_back("return");
                 }
@@ -188,17 +190,17 @@ void conversion(){
                     if(tac[i][1] == "STR")
                         continue;
                     if(tac[i].size() == 3){
-                        local[tac[i][2]].first = local_idx++;
+                        local[tac[i][2]].first = to_string(local_idx++);
                         local[tac[i][2]].second = tac[i][1];
                     }
                 }
                 else if(tac[i][0] == "param"){
-                    pair<pair<int, string>, string> type_a = get_type(tac[i][1], tac[i][2]);
-                    vm.push_back("push " + type_a.second + " " + to_string(type_a.first.first) + " " + type_a.first.second);
+                    pair<pair<string, string>, string> type_a = get_type(tac[i][1], tac[i][2]);
+                    vm.push_back("push " + type_a.second + " " + type_a.first.first + " " + type_a.first.second);
                 }
                 else if(tac[i][0] == "input"){
-                    pair<pair<int, string>, string> type_a = get_type(tac[i][1], tac[i][2]);
-                    vm.push_back("scan " + type_a.second + " " + to_string(type_a.first.first) + " " + tac[i][2]);
+                    pair<pair<string, string>, string> type_a = get_type(tac[i][1], tac[i][2]);
+                    vm.push_back("scan " + type_a.second + " " + type_a.first.first + " " + tac[i][2]);
                 }
                 else if(tac[i][0] == "output"){
                     if(tac[i][2] == "STR"){
@@ -209,8 +211,8 @@ void conversion(){
                         vm.push_back("push data " + to_string(strings[tac[i][1]]) + " STR");
                     }
                     else{
-                        pair<pair<int, string>, string> type_a = get_type(tac[i][1], tac[i][2]);
-                        vm.push_back("push " + type_a.second + " " + to_string(type_a.first.first) + " " + type_a.first.second);
+                        pair<pair<string, string>, string> type_a = get_type(tac[i][1], tac[i][2]);
+                        vm.push_back("push " + type_a.second + " " + type_a.first.first + " " + type_a.first.second);
                     }
                     vm.push_back("print " + tac[i][2]);
                 }
@@ -218,7 +220,7 @@ void conversion(){
             else if(tac[i].size() == 4){
                 if(tac[i][1] == "arg"){
                     // function parameters
-                    argument[tac[i][2]].first = arg_idx++;
+                    argument[tac[i][2]].first = to_string(arg_idx++);
                     argument[tac[i][2]].second = tac[i][2];
                 }
                 else{
@@ -228,13 +230,13 @@ void conversion(){
                         strings[tac[i][0]] = str_idx++;
                     }
                     else{
-                        pair<pair<int, string>, string> type_a = get_type(tac[i][0], tac[i][3]);
-                        pair<pair<int, string>, string> type_b = get_type(tac[i][2], tac[i][3]);
+                        pair<pair<string, string>, string> type_a = get_type(tac[i][0], tac[i][3]);
+                        pair<pair<string, string>, string> type_b = get_type(tac[i][2], tac[i][3]);
                         if(type_b.second == "constant")
                             vm.push_back("push " + type_b.second + " " + tac[i][2] + " " + type_b.first.second);
                         else
-                            vm.push_back("push " + type_b.second + " " + to_string(type_b.first.first) + " " + type_b.first.second);
-                        vm.push_back("pop " + type_a.second + " " + to_string(type_a.first.first) + " " + type_a.first.second);
+                            vm.push_back("push " + type_b.second + " " + type_b.first.first + " " + type_b.first.second);
+                        vm.push_back("pop " + type_a.second + " " + type_a.first.first + " " + type_a.first.second);
                     }
                 }
             }
@@ -243,23 +245,23 @@ void conversion(){
                 auto a = get_type(tac[i][0], tac[i][4]);
                 auto b = get_type(tac[i][3], tac[i][4]);
 
-                vm.push_back("push " + b.second + " " + to_string(b.first.first) + " " + a.first.second);
+                vm.push_back("push " + b.second + " " + b.first.first + " " + a.first.second);
                 if(tac[i][2] == "-")
-                    vm.push_back("neg");
+                    vm.push_back("neg " + tac[i][4]);
                 else
-                    vm.push_back("not");
-                vm.push_back("pop " + a.second + " " + to_string(a.first.first) + " " + a.first.second);
+                    vm.push_back("not " + tac[i][4]);
+                vm.push_back("pop " + a.second + " " + a.first.first + " " + a.first.second);
             }
             else if(tac[i].size() == 6){
                 if(tac[i][2] == "@call"){
                     vm.push_back("call " + tac[i][3] + " " + tac[i][5]);
-                    pair<pair<int, string>, string> a = get_type(tac[i][0], tac[i][4]);
+                    pair<pair<string, string>, string> a = get_type(tac[i][0], tac[i][4]);
                     vm.push_back("push argument 0 " + tac[i][4]);
-                    vm.push_back("pop " + a.second + " " + to_string(a.first.first) + " " + a.first.second);
+                    vm.push_back("pop " + a.second + " " + a.first.first + " " + a.first.second);
                 }
                 else if(tac[i][3] == "["){
                     // array declarations
-                    local[tac[i][2]].first = local_idx;
+                    local[tac[i][2]].first = to_string(local_idx);
                     local_idx += stoi(tac[i][4]);
                     local[tac[i][2]].second = tac[i][1];
                 }
@@ -269,17 +271,17 @@ void conversion(){
                     // arr [ 8 ] = @t0 INT
                     auto a = get_type(tac[i][5], tac[i][6]);
                     auto b = get_type(tac[i][2], "INT");
-                    vm.push_back("push " + a.second + " " + to_string(a.first.first) + " " + a.first.second);
+                    vm.push_back("push " + a.second + " " + a.first.first + " " + a.first.second);
 
-                    vm.push_back("push constant " + to_string(local[tac[i][0]].first) + " INT");
-                    vm.push_back("push " + b.second + " " + to_string(b.first.first) + " " + b.first.second);
-                    vm.push_back("push " + b.second + " " + to_string(b.first.first) + " " + b.first.second);
-                    vm.push_back("add " + tac[i][6]);
-                    vm.push_back("push " + b.second + " " + to_string(b.first.first) + " " + b.first.second);
-                    vm.push_back("push " + b.second + " " + to_string(b.first.first) + " " + b.first.second);
-                    vm.push_back("add " + tac[i][6]);
-                    vm.push_back("add " + tac[i][6]);
-                    vm.push_back("add " + tac[i][6]);
+                    vm.push_back("push constant " + local[tac[i][0]].first + " INT");
+                    vm.push_back("push " + b.second + " " + b.first.first + " " + b.first.second);
+                    vm.push_back("push " + b.second + " " + b.first.first + " " + b.first.second);
+                    vm.push_back("add INT");
+                    vm.push_back("push " + b.second + " " + b.first.first + " " + b.first.second);
+                    vm.push_back("push " + b.second + " " + b.first.first + " " + b.first.second);
+                    vm.push_back("add INT");
+                    vm.push_back("add INT");
+                    vm.push_back("add INT");
                     // vm.push_back("pop pointer 0");
                     vm.push_back("pop that 0 " + tac[i][6]);
                 }
@@ -288,22 +290,22 @@ void conversion(){
                     auto a = get_type(tac[i][0], tac[i][6]);
                     auto b = get_type(tac[i][4], "INT");
 
-                    vm.push_back("push constant " + to_string(local[tac[i][2]].first) + " " + tac[i][6]);
-                    vm.push_back("push " + b.second + " " + to_string(b.first.first) + " " + b.first.second);
-                    vm.push_back("push " + b.second + " " + to_string(b.first.first) + " " + b.first.second);
-                    vm.push_back("add " + tac[i][6]);
-                    vm.push_back("push " + b.second + " " + to_string(b.first.first) + " " + b.first.second);
-                    vm.push_back("push " + b.second + " " + to_string(b.first.first) + " " + b.first.second);
-                    vm.push_back("add " + tac[i][6]);
-                    vm.push_back("add " + tac[i][6]);
-                    vm.push_back("add " + tac[i][6]);
+                    vm.push_back("push constant " + local[tac[i][2]].first + " " + tac[i][6]);
+                    vm.push_back("push " + b.second + " " + b.first.first + " " + b.first.second);
+                    vm.push_back("push " + b.second + " " + b.first.first + " " + b.first.second);
+                    vm.push_back("add INT");
+                    vm.push_back("push " + b.second + " " + b.first.first + " " + b.first.second);
+                    vm.push_back("push " + b.second + " " + b.first.first + " " + b.first.second);
+                    vm.push_back("add INT");
+                    vm.push_back("add INT");
+                    vm.push_back("add INT");
                     // vm.push_back("pop pointer 0");
                     vm.push_back("push that 0 " + tac[i][6]);
-                    vm.push_back("pop " + a.second + " " + to_string(a.first.first) + " " + tac[i][6]);
+                    vm.push_back("pop " + a.second + " " + a.first.first + " " + tac[i][6]);
                 }
                 else{
                     // if t0 goto L1 else goto L2
-                    pair<pair<int, string>, string> type_a = get_type(tac[i][1], "null");
+                    pair<pair<string, string>, string> type_a = get_type(tac[i][1], "null");
                     // vm.push_back("push constant 0 INT");
                     // vm.push_back("push " + type_a.second + " " + to_string(type_a.first.first) + " " + type_a.first.second);
                     // vm.push_back("eq INT");
---
+++ Compiler/y.tab.h
@@ -0,0 +1,193 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 1
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    INT = 258,
+    CHAR = 259,
+    FLOAT = 260,
+    STRING = 261,
+    VOID = 262,
+    RETURN = 263,
+    INT_NUM = 264,
+    FLOAT_NUM = 265,
+    ID = 266,
+    LEFTSHIFT = 267,
+    RIGHTSHIFT = 268,
+    LE = 269,
+    GE = 270,
+    EQ = 271,
+    NE = 272,
+    GT = 273,
+    LT = 274,
+    AND = 275,
+    OR = 276,
+    NOT = 277,
+    ADD = 278,
+    SUBTRACT = 279,
+    DIVIDE = 280,
+    MULTIPLY = 281,
+    MODULO = 282,
+    BITAND = 283,
+    BITOR = 284,
+    NEGATION = 285,
+    XOR = 286,
+    STR = 287,
+    CHARACTER = 288,
+    CC = 289,
+    OC = 290,
+    CS = 291,
+    OS = 292,
+    CF = 293,
+    OF = 294,
+    COMMA = 295,
+    COLON = 296,
+    SCOL = 297,
+    OUTPUT = 298,
+    INPUT = 299,
+    SWITCH = 300,
+    CASE = 301,
+    BREAK = 302,
+    DEFAULT = 303,
+    IF = 304,
+    ELIF = 305,
+    ELSE = 306,
+    WHILE = 307,
+    FOR = 308,
+    CONTINUE = 309,
+    ASSIGN = 310
+  };
+#endif
+/* Tokens.  */
+#define INT 258
+#define CHAR 259
+#define FLOAT 260
+#define STRING 261
+#define VOID 262
+#define RETURN 263
+#define INT_NUM 264
+#define FLOAT_NUM 265
+#define ID 266
+#define LEFTSHIFT 267
+#define RIGHTSHIFT 268
+#define LE 269
+#define GE 270
+#define EQ 271
+#define NE 272
+#define GT 273
+#define LT 274
+#define AND 275
+#define OR 276
+#define NOT 277
+#define ADD 278
+#define SUBTRACT 279
+#define DIVIDE 280
+#define MULTIPLY 281
+#define MODULO 282
+#define BITAND 283
+#define BITOR 284
+#define NEGATION 285
+#define XOR 286
+#define STR 287
+#define CHARACTER 288
+#define CC 289
+#define OC 290
+#define CS 291
+#define OS 292
+#define CF 293
+#define OF 294
+#define COMMA 295
+#define COLON 296
+#define SCOL 297
+#define OUTPUT 298
+#define INPUT 299
+#define SWITCH 300
+#define CASE 301
+#define BREAK 302
+#define DEFAULT 303
+#define IF 304
+#define ELIF 305
+#define ELSE 306
+#define WHILE 307
+#define FOR 308
+#define CONTINUE 309
+#define ASSIGN 310
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+
+union YYSTYPE
+{
+#line 102 "parser.y" /* yacc.c:1909  */
+
+    struct node { 
+        char lexeme[100];
+        int line_number;
+        char type[100];
+        char if_body[5];
+        char elif_body[5];
+		char else_body[5];
+        char loop_body[5];
+        char parentNext[5];
+        char case_body[5];
+        char id[5];
+        char temp[5];
+        int nParams;
+    } node;
+
+#line 181 "y.tab.h" /* yacc.c:1909  */
+};
+
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
